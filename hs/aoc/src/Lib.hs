cf17ad46be47e2b578af3a43953630fe0ae6ea65
Hemanth Kapila <kapilash@outlook.com>
2022-12-22 15:37:52 +0530

    day 3

--- hs/aoc/src/Lib.hs
+++ hs/aoc/src/Lib.hs
@@ -3,35 +3,40 @@ module Lib
     maxCalorie,
     max3Calories,
     day2Puzzle1,
+    day3Puzzle1,
+    day3Puzzle2,
     day2Puzzle2
     ) where
 
 import qualified Data.List as Lst 
 import Text.Parsec
+import Data.Char 
+import qualified Data.Set as Set
+import qualified Data.List as Lst
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
-type CalParser s = Parsec s Int
+type CalParser  = Parsec String Int
 
-eol :: Stream s m Char => ParsecT s u m ()
+eol :: Monad m => ParsecT String u m ()
 eol = do
     newline <|> crlf
     return ()
 
-calorieLine :: Stream s m Char => ParsecT s u m Int
+calorieLine :: Monad m => ParsecT String u m Int
 calorieLine = do
    num <- many1 digit
    eol
    return $ read num
 
-calorieChunk :: Stream s m Char => ParsecT s u m Int
+calorieChunk :: Monad m => ParsecT String u m Int
 calorieChunk = do
   calories <- many1 calorieLine
   eol
   return $ sum calories
 
-elfCalorie1 :: Stream s m Char => ParsecT s Int m ()
+elfCalorie1 :: Monad m => ParsecT String Int m ()
 elfCalorie1 = do
   next <- calorieChunk
   modifyState (\x -> max x next)
@@ -43,19 +48,19 @@ changeStateIfNeeded curr (f, s, t) = if curr >= f then (curr, f, s)
                                           else if curr >= t then (f, s, curr)
                                                else (f, s, t)
 
-elfCalorie2 :: Stream s m Char => ParsecT s (Int,Int,Int) m ()
+elfCalorie2 :: Monad m => ParsecT String (Int,Int,Int) m ()
 elfCalorie2 = do
   next <- calorieChunk
   modifyState (changeStateIfNeeded next)
   return ()
 
-allCalories1 :: Stream s m Char => ParsecT s Int m Int
+allCalories1 :: Monad m => ParsecT String Int m Int
 allCalories1 = do
   many1 (try elfCalorie1)
   i <- getState
   return i
 
-allCalories2 :: Stream s m Char => ParsecT s (Int,Int,Int) m (Int,Int,Int)
+allCalories2 :: Monad m => ParsecT String (Int,Int,Int) m (Int,Int,Int)
 allCalories2 = do
   many1 (try elfCalorie2)
   i <- getState
@@ -115,44 +120,44 @@ p2outcome Scissors Tie = 3 + (shapeScore Scissors)
 p2outcome Scissors Lose = 0 + (shapeScore Paper)
 
 
-genTurn :: Char -> Day2 -> Stream s m Char => ParsecT s u m Day2
+genTurn :: Monad m => Char -> Day2 -> ParsecT String u m Day2
 genTurn c d = do
    char c
    return d
 
-theirTurn :: Stream s m Char => ParsecT s u m Day2
+theirTurn :: Monad m => ParsecT String u m Day2
 theirTurn = do
    theirs <- (genTurn 'A' Rock) <|> (genTurn 'B' Paper) <|> (genTurn 'C' Scissors)
    many1 space
    return theirs 
 
-genTurn2 :: Char -> Day2P2 -> Stream s m Char => ParsecT s u m Day2P2
+genTurn2 :: Monad m => Char -> Day2P2 -> ParsecT String u m Day2P2
 genTurn2 c d = do
     char c
     return d
 
-myTurn2 :: Stream s m Char => ParsecT s u m Day2P2
+myTurn2 :: Monad m => ParsecT String u m Day2P2
 myTurn2 = do
     mine <- (genTurn2 'X' Lose) <|> (genTurn2 'Y' Tie) <|> (genTurn2 'Z' Win)
     eol
     return mine
 
 
-myTurn :: Stream s m Char => ParsecT s u m Day2
+myTurn :: Monad m => ParsecT String u m Day2
 myTurn = do
    mine <- (genTurn 'X' Rock) <|> (genTurn 'Y' Paper) <|> (genTurn 'Z' Scissors)
    eol
    return mine
 
 
-oneGame :: Stream s m Char => ParsecT s Int m ()
+oneGame :: Monad m => ParsecT String Int m ()
 oneGame = do
    theirChoice <- theirTurn
    myChoice <- myTurn
    modifyState (\x -> x + (outcome theirChoice myChoice) + (shapeScore myChoice))
    return ()
 
-allGames :: Stream s m Char => ParsecT s Int m Int
+allGames :: Monad m => ParsecT String Int m Int
 allGames = do
   many1 oneGame
   eof
@@ -166,14 +171,14 @@ day2Puzzle1 f = do
     Left err -> print err
     Right v  -> print v
 
-oneGame2 :: Stream s m Char => ParsecT s Int m ()
+oneGame2 :: Monad m => ParsecT String Int m ()
 oneGame2 = do
    theirChoice <- theirTurn
    myChoice <- myTurn2
    modifyState (\x -> x + (p2outcome theirChoice myChoice))
    return ()
 
-allGames2 :: Stream s m Char => ParsecT s Int m Int
+allGames2 :: Monad m => ParsecT String Int m Int
 allGames2 = do
   many1 oneGame2
   eof
@@ -186,3 +191,39 @@ day2Puzzle2 f = do
    case (runParser allGames2 0 f input) of
     Left err -> print err
     Right v  -> print v
+
+priority :: Char -> Int
+priority c = if c >= 'a' && c <= 'z' then 1 + (ord c) - (ord 'a')
+                 else if c >= 'A' && c <= 'Z' then 27 + (ord c) - (ord 'A')
+                      else 0
+
+rucksackPriority :: String -> Int
+rucksackPriority s = let 
+                    (left, right) = Lst.splitAt (div (length s)  2) s
+                   in
+                   priority . Set.elemAt 0 $ Set.intersection (Set.fromList left) (Set.fromList right) 
+
+day3Puzzle1 :: FilePath -> IO ()
+day3Puzzle1 f = do
+   input <- readFile f
+   print $ sum $ map rucksackPriority $ lines input
+
+commonChars :: [String] -> Set.Set Char
+commonChars  = foldl (\x y -> Set.intersection x (Set.fromList y)) initialSet
+            where
+                initialSet = Set.union (Set.fromList ['a' .. 'z']) (Set.fromList ['A' .. 'Z'])
+
+groupScore :: [String] -> Int
+groupScore [] = 0
+groupScore s = let 
+                (curr, next) = Lst.splitAt 3 s
+                currValue = priority . Set.elemAt 0 $ commonChars curr
+              in
+                case next of
+                   [] -> currValue
+                   _ -> currValue + (groupScore next)
+
+day3Puzzle2 :: FilePath -> IO ()
+day3Puzzle2 f = do
+    input <- readFile f
+    print $ groupScore (lines input)
